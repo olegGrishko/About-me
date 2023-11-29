9b10fcd899dcb3af3c28d41680392c08335609c5
Oleg Grishko <oleggr-brd@yandex.ru>
2023-11-29 19:01:42 +0300

    Added input userName for chat

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 function send() {
     let message = document.getElementById('message').value;
+    let userName = document.getElementById('userName').value;
+    let historyMes = '';
+
+    if (userName == '') {
+        document.getElementById('errors').innerText = 'Вы забыли ввести свое имя';
+        return
+    }
 
     if (message == '') {
         document.getElementById('errors').innerText = 'Вы забыли ввести сообщение';
+        historyMes = '';
         return
     }
+    if (message !== '') {
+        historyMes = document.getElementById('messages').value;
+        console.log(historyMes);
+    }
+
     document.getElementById('errors').innerText = '';
     document.getElementById('message').value = '';
-    document.getElementById('messages').innerText = message;
+    document.getElementById('messages').innerHTML = userName + ': ' + message + ' <BR> ' + historyMes;
 }
