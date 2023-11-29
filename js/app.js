9b4f797400d124cccb3dbc387633964779ba8140
Oleg Grishko <oleggr-brd@yandex.ru>
2023-11-29 19:21:01 +0300

    js ready

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 function send() {
     let message = document.getElementById('message').value;
     let userName = document.getElementById('userName').value;
-    let historyMes = '';
+    let historyMes = document.getElementById('messages').innerHTML;
 
     if (userName == '') {
-        document.getElementById('errors').innerText = 'Вы забыли ввести свое имя';
-        return
+        //document.getElementById('errors').innerText = 'Вы забыли ввести свое имя';
+        userName = 'Гость';
+        //return
     }
 
     if (message == '') {
         document.getElementById('errors').innerText = 'Вы забыли ввести сообщение';
-        historyMes = '';
         return
     }
-    if (message !== '') {
-        historyMes = document.getElementById('messages').value;
-        console.log(historyMes);
-    }
+
 
     document.getElementById('errors').innerText = '';
     document.getElementById('message').value = '';
