a5895ed7d5a4af1e69529084a9969f9c255eefa4
Oleg Grishko <oleggr-brd@yandex.ru>
2023-12-02 00:03:28 +0300

    php ready

--- chat.php
+++ chat.php
@@ -8,5 +8,37 @@ if (isset($_GET['message'])) {
     die();
 }
 
-$messages = file_get_contents('chat.txt');
-echo $messages;
+/*$messages = file_get_contents('chat.txt');
+print($messages);
+$messages_rev = array_reverse($messages);
+echo $messages_rev;*/
+
+/*
+$content = file_get_contents('chat.txt');
+$content1 = explode("\n", $content);
+krsort($content1);
+//var_dump($content1);
+foreach ($content1 as $value) {
+    if (!empty($value)) {
+        echo $value . "</br>";
+    }
+}
+*/
+//------------------------------work---------------------------
+?>
+<ul style="list-style: none;">
+<?php
+	$file_text = file_get_contents( 'chat.txt');
+	$text_list = explode( "\n", $file_text );
+	krsort( $text_list );
+	foreach ( $text_list as $text ) {
+		if ( ! empty ( $text ) ) {
+		?>
+			<li> <?php echo $text; ?></li>
+		<?php
+		}
+
+	}
+
+?>
+</ul>
--- js/app2.js
+++ js/app2.js
@@ -1,12 +1,26 @@
 getMessage();
 setInterval(getMessage, 2000);
-
+//------work
 function getMessage() {
     (async () => {
         const response = await fetch('chat.php');
         const answer = await response.text();
-        document.getElementById('messages').innerText = answer;
+        document.getElementById('messages').innerHTML = answer;
         //console.log("222");
     }
     )();
 }
+//----
+/*function getMessage() {
+    let arr = [];
+    let rev_arr = [];
+    (async () => {
+        const response = await fetch('chat.php');
+        const answer = await response.text();
+        arr.push(answer);
+        rev_arr = arr.reverse();
+        //  document.getElementById('messages').innerText = answer;
+        console.log(rev_arr);
+    }
+    )();
+}*/
